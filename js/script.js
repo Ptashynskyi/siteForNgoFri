777cdc7ab0260e41211411f15b98c5a081c135f2
Ptashynskyi <n.ptashynskyi@gmail.com>
2019-06-27 17:10:13 +0300

    add timer logic

--- js/script.js
+++ js/script.js
@@ -32,4 +32,55 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+    let deadline = '2019-06-30';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t / 100) % 60),
+            minutes = Math.floor((t / 100 / 60) % 60),
+            hours = Math.floor((t / (1000 * 60 * 60)));
+
+        return {
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+
+
+            function addZeroInTimer(num) {
+                if (num <= 9) {
+                    return "0" + num;
+                } else {
+                    return num;
+                }
+            };
+
+            hours.textContent = addZeroInTimer(t.hours);
+            minutes.textContent = addZeroInTimer(t.minutes);
+            seconds.textContent = addZeroInTimer(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minute.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+    }
+
+    setClock('timer', deadline);
+
 });
